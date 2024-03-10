c982a28ce6714c85a10ea5d234c91bc4a41933c6
iambawala <162889857+iambawala@users.noreply.github.com>
2024-03-11 00:35:52 +0530

    review, courses, study material, FAQ, & pdf added

---
+++ script.js
@@ -0,0 +1,17 @@
+function searchFunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('searchInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById('searchResults');
+    li = ul.getElementsByTagName('div');
+
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName('h3')[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = '';
+        } else {
+            li[i].style.display = 'none';
+        }
+    }
+}
